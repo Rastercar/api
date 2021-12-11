0dea3e763cb8ffc44575acb2b4d4173040c782f3
VitAndrGuid <vitor.guidorizzi@hotmail.com>
2021-12-10 23:54:18 -0400

    chore: remove examples app.controller and app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,9 @@ import { GraphqlModule } from './graphql/graphql.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { ConfigModule } from './config/config.module'
 import { OrmModule } from './database/orm.module'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { Module } from '@nestjs/common'
 
 @Module({
-  imports: [OrmModule, ConfigModule, AuthModule, GraphqlModule],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [OrmModule, ConfigModule, AuthModule, GraphqlModule]
 })
 export class AppModule {}
